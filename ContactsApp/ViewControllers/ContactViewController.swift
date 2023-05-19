475a98135077e29a6654023f58448aab1ed6e8c5
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-19 19:39:09 +0300

    ContactsVC code added

--- ContactsApp/ViewControllers/ContactViewController.swift
+++ ContactsApp/ViewControllers/ContactViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AlamofireImage
 
-final class ContactViewController: UIViewController {
-
+final class DetailContactViewController: UIViewController {
+    
+    @IBOutlet var userImageView: UIImageView!
+    @IBOutlet var firstNameLabel: UILabel!
+    @IBOutlet var lastNameLabel: UILabel!
+    
+    var user: User!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setValues(with: user)
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        userImageView.layer.cornerRadius = userImageView.bounds.height / 2
+    }
+    
+    private func setValues(with user: User) {
+        firstNameLabel.text = user.name.first
+        lastNameLabel.text = user.name.last
+        
+        guard let imageURL = URL(string: user.picture.large) else { return }
+        userImageView.af.setImage(withURL: imageURL)
+        
+    }
 }
 
